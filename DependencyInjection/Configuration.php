592d94d95fb7f4a7e56d64f91cccd30bd15e8112
2latlantik <nicolascoudenne@gmail.com>
2020-02-28 12:41:32 +0100

    Compatibility with SF 5

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -15,8 +15,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('recaptcha');
+        $treeBuilder = new TreeBuilder('recaptcha');
+        $rootNode = $treeBuilder->getRootNode();
         $rootNode
             ->children()
             ->scalarNode('key')
